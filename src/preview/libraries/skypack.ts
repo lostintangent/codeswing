fa1d8646af30f3278866b8b1c0fdbff6e0da91bc
Jonathan Carter <joncart@microsoft.com>
2021-07-01 20:06:36 +0000

    Fixing CSS/JSON imports

--- src/preview/libraries/skypack.ts
+++ src/preview/libraries/skypack.ts
@@ -18,9 +18,7 @@ async function getModules(searchString: string) {
 
 async function hasDefaultExport(moduleName: string) {
   const moduleUrl = getModuleUrl(moduleName);
-  const { data } = await axios.get(
-    `${moduleUrl}?meta`
-  );
+  const { data } = await axios.get(`${moduleUrl}?meta`);
   return data.packageExports?.["."]?.hasDefaultExport || false;
 }
 
@@ -29,7 +27,7 @@ const IMPORT_SUBSTITION = `$1$2https://cdn.skypack.dev/$3$2`;
 export function processImports(code: string) {
   return code
     .replace(IMPORT_PATTERN, IMPORT_SUBSTITION)
-    .replace(/\.\/(\S+)\.(svelte|vue|jsx|tsx)/g, "./$1.js?type=$2");
+    .replace(/\.\/(\S+)\.(svelte|vue|jsx|tsx|json|css)/g, "./$1.js?type=$2");
 }
 
 const DEFAULT_MODULES = [
